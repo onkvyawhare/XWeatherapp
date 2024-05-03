3f711c9f2eeee1ed9c1ab02c5396d73eb11a89c1
onkvyawhare <onkvyawhare@gmail.com>
2024-05-03 15:52:41 +0530

    final commit

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
+import {useState} from "react";
 import './App.css';
 
 function App() {
+
+
+  const [city, setCity] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const API_KEY = "d5e1418f928c4fe28b4202510240105";
+
+
+  const handleSearch=async () =>{
+    if(!city) return;
+
+    setIsLoading(true);
+    setError(null);
+
+    try {
+      const response = await fetch(
+        `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}`
+      );
+      const data = await response.json();
+      if (data.error) {
+        throw new Error(data.error.message);
+      }
+      setWeatherData(data);
+    } catch (error) {
+      setError("Failed to fetch weather data");
+      alert("Failed to fetch weather data");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weather Application</h1>
+      <div className="search-container">
+        <input
+          type="text"
+          placeholder="Enter city name"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <button onClick={handleSearch}>Search</button>
+      </div>
+      {isLoading && <p>Loading data...</p>}
+      {error && <p>{error}</p>}
+      {weatherData && (
+        <div className="weather-cards">
+          <div className="weather-card">
+            <h2>Temperature</h2>
+            <p>{weatherData.current.temp_c}°C</p>
+          </div>
+          <div className="weather-card">
+            <h2>Humidity </h2>
+            <p>{weatherData.current.humidity}%</p>
+          </div>
+          <div className="weather-card">
+            <h2>Condition </h2>
+            <p>{weatherData.current.condition.text}</p>
+          </div>
+          <div className="weather-card">
+            <h2>Wind Speed </h2>
+            <p>{weatherData.current.wind_kph} km/h</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
